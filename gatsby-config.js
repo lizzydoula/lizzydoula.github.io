7b9072d3ee2fbff574cddad1055fbfeb5726d43e
Andrei Hameza <andrey.hameza@exness.com>
2018-12-23 21:21:11 +0200

    Chnage favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: 'standalone',
-        icon: 'src/favicon.png',
+        icon: 'src/favicon.jpg',
       },
     },
     'gatsby-plugin-offline',
