e8e3a0440d256afd8101e43fce0bb86a8895a39b
Andrei Hameza <andrey.hameza@exness.com>
2019-01-01 18:41:41 +0200

    Update site title and description

--- config/website.js
+++ config/website.js
@@ -1,11 +1,11 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  siteTitle: 'Доула Лиза', // Navigation and Site Title
-  siteTitleAlt: 'Доула Лиза', // Alternative Site title for SEO
+  siteTitle: 'Доула Лиза на Кипре: курсы для беременных, сопровождение в родах и послеродовое восстановление', // Navigation and Site Title
+  siteTitleAlt: 'Доула Лиза на Кипре: курсы для беременных, сопровождение в родах и послеродовое восстановление', // Alternative Site title for SEO
   siteUrl: 'https://doulalizzy.com', // Domain of your site. No trailing slash!
   siteLanguage: 'ru', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
-  siteDescription: 'Рядом с тобой в беременность, в родах и после',
+  siteDescription: 'Доула Лиза на Кипре: курсы и нейрографика для беременных, индивидуальные консультации, сопровождение в родах, послеродовое восстановление и пеленание, массаж живота, онлайн поддержка, помощь по грудному вскармливанию',
   author: 'Lizaveta Schaefer', // Author for schemaORGJSONLD
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   // userTwitter: '@emma', // Twitter Username
